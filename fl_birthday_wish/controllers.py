e9c2512bbc1249245d55ec8f97c8142664a8b1ee
Denis Vermylen <dve@odoo.com>
2020-07-29 15:21:51 +0200

    betterslowpoke4

--- fl_birthday_wish/controllers.py
+++ fl_birthday_wish/controllers.py
@@ -1,37 +1,38 @@
 import logging
-# import time
+import random
+import time
 
 from odoo import http
 #from odoo.http import request
 
-_logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class worker(http.Controller):
+    def coucou(a):
+        fake_sql_reqs = int(random.random() * 100)
+        breaks = sorted([random.randrange(1, a) for i in range(fake_sql_reqs)], reverse=1)
+        for b in breaks:
+            while a > b:
+                a -= 1
+            time.sleep(random.random() / 100)
+
     @http.route('/slowpoke1', type='http', auth='none')
     def slowpoke1(self, **params):
-        a = 0
-        while a < 10**7:
-            a += 1
+        self.coucou(10**7)
         return '200 OK'
 
     @http.route('/slowpoke2', type='http', auth='none')
     def slowpoke2(self, **params):
-        a = 0
-        while a < 10**8:
-            a += 1
+        self.coucou(10**8)
         return '200 OK'
 
     @http.route('/slowpoke3', type='http', auth='none')
     def slowpoke3(self, **params):
-        a = 0
-        while a < 10**9:
-            a += 1
+        self.coucou(10**9)
         return '200 OK'
 
     @http.route('/slowpoke4', type='http', auth='none')
     def slowpoke4(self, **params):
-        a = 0
-        while a < 10**10:
-            a += 1
+        self.coucou(10**10)
         return '200 OK'
